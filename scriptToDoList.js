790ca2d3b0dfe24c6b69020e70422ebde4473a3e
Juan Carlos Ramírez García <juanca210503@gmail.com>
2025-05-27 10:09:17 -0600

    Local Storage implementado

--- scriptToDoList.js
+++ scriptToDoList.js
@@ -6,23 +6,50 @@ const app=createApp({
         const nuevaTarea = ref('');
         const tareas = ref([]);
 
+        const tareasGuardadas=localStorage.getItem('tareas');
+        if(tareasGuardadas){
+            tareas.value=JSON.parse(tareasGuardadas);
+
+        }
+
+
         const agregarTarea=()=>{
             if(nuevaTarea.value.trim()!==''){
-                tareas.value.push(nuevaTarea.value);
+                tareas.value.push({ 
+                    texto: nuevaTarea.value, 
+                    completado: false });
+                localStorage.setItem('tareas', JSON.stringify(tareas.value));
                 nuevaTarea.value='';
             }
         }
 
         const eliminarTarea=(index)=>{
             tareas.value.splice(index,1);
+            localStorage.setItem('tareas', JSON.stringify(tareas.value));
+
         }
+
+        const clearAll = ()=>{
+            tareas.value=[];
+            localStorage.setItem('tareas', JSON.stringify(tareas.value));
+
+        }
+
+        const clearCompleted = () => {
+            tareas.value = tareas.value.filter(tarea => !tarea.completado);
+            localStorage.setItem('tareas', JSON.stringify(tareas.value));
+
+        }
+        
 
 
         return{
             nuevaTarea,
             tareas,
             agregarTarea,
-            eliminarTarea
+            eliminarTarea,
+            clearAll,
+            clearCompleted
             
         }
     }
